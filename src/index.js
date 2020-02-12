c79a4526a0bcd126a3ae9e57daebf190500fe90b
ilikecoffee18 <cherish.kim@students.makeschool.com>
2020-02-11 23:40:26 -0800

    majority of tutorial finished now woot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'dotenv/config' //should precede local file imports
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
 
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(3000, () =>
-    console.log('Example app listening on port 3000!'),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 )
 
 console.log('Hello ever running Node.js project')
